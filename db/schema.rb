a0413f0058db77ce4e1a9263a6580ccee90ab58b
Jonathan Lau <jonhlau28@gmail.com>
2019-04-25 01:20:02 -0700

    add user servers

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190425064513) do
+ActiveRecord::Schema.define(version: 20190425081252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "servers", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_servers_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "screenname", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -25,6 +34,8 @@ ActiveRecord::Schema.define(version: 20190425064513) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["screenname"], name: "index_users_on_screenname", unique: true
   end
 
+  add_foreign_key "servers", "users"
 end
