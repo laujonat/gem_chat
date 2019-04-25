74786ca418740a9961bad233abfa6f1f0fe898f9
Jonathan Lau <jonhlau28@gmail.com>
2019-04-25 01:44:52 -0700

    Create user and server seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+SERVER_NAMES = 10.times.map { Faker::Internet.domain_word }
+
+SPLIT = 0.4
+OFFSET = SERVER_NAMES.length * SPLIT
+
+SERVER_USERS = {
+  'mr_apollo@example.com' => SERVER_NAMES[0, OFFSET],
+  'ms_apollo@example.com' => SERVER_NAMES[OFFSET..-1]
+}
+
+SERVER_USERS.each do |email, servers|
+  user = User.new(email: email, screenname: Faker::Games::WorldOfWarcraft.hero, password: 'secret')
+  servers.each { |name| user.servers.build(name: name) }
+  user.save!
+end 
