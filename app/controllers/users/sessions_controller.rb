53f27d82e50bfe0de2fcc1b3ab564b3ed315d60e
Jonathan Lau <jonhlau28@gmail.com>
2019-04-25 17:00:39 -0700

    helper user methods and session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,25 +3,20 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
   def new
     super
   end
 
-  # POST /resource/sign_in
   def create
     super
   end
 
-  # DELETE /resource/sign_out
   def destroy
-    current_user.invalidate_all_sessions!
-    super
+    logout! if current_user?
   end
 
   # protected
 
-  # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :screenname, :email])
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  validates :screenname, :session_token, :email, presence: true, uniqueness: true
 
   has_many :servers 
 
-  def invalidate_all_sessions!
-    self.session_token = SecureRandom.hex
+  def self.find_by_screen_name(screenname)
+    User.find_by(screenname: screenname)
+  end 
+
+  def self.find_by_email(email)
+    User.find_by(email: email)
   end
 end
