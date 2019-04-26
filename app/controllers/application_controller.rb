275ece8da6a02ed53ece8917022a870116a3ad8e
Jonathan Lau <jonhlau28@gmail.com>
2019-04-25 18:05:53 -0700

    Redirect sign out to new_user_session_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?  
+
+  private 
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 
   def configure_permitted_parameters
     added_attrs = [:screenname, :email, :password, :password_confirmation, :remember_me]
