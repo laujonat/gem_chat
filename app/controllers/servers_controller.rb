e9e7ffd42a208abadc015face292a04fe9c4f7fe
Jonathan Lau <jonhlau28@gmail.com>
2019-04-27 20:56:17 -0700

    boilerplate controller generation

---
+++ app/controllers/servers_controller.rb
@@ -0,0 +1,13 @@
+class ServersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,32 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'channels/index'
+
+  get 'channels/new'
+
+  get 'channels/create'
+
+  get 'channels/show'
+
+  get 'channels/destroy'
+
+  get 'messages/index'
+
+  get 'messages/new'
+
+  get 'messages/create'
+
+  get 'messages/show'
+
+  get 'servers/index'
+
+  get 'servers/new'
+
+  get 'servers/create'
+
+  get 'servers/show'
+
   devise_for :users
   root 'static_pages#root'
 
