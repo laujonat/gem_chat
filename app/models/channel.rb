a10fe65b8276fe29baa2161290257e383717f1b2
Jonathan Lau <jonhlau28@gmail.com>
2019-04-27 21:39:31 -0700

    channel validation spec

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,5 +1,5 @@
 class Channel < ApplicationRecord
-  validates :name, :server_id, presence: true
+  validates :name, presence: true
   belongs_to :server
   has_many :messages, dependent: :destroy
 end
--- spec/models/channel_spec.rb
+++ spec/models/channel_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Channel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do 
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to belong_to :server }
+    it { is_expected.to have_many :messages}
+  end
 end
