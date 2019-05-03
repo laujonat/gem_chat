fbb30bcfd8564d977589d2d6ad800996984705ea
Jonathan Lau <jonhlau28@gmail.com>
2019-05-02 19:01:30 -0700

    Add additional model spec and remove application stylesheet

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do 
+    it { is_expected.to validate_presence_of :body }
+    it { is_expected.to validate_presence_of :user_id }
+    it { is_expected.to validate_presence_of :channel_id }
+    it { is_expected.to belong_to :user }
+    it { is_expected.to belong_to :channel }
+  end 
 end
