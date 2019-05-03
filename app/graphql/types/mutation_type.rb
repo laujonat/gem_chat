ee15b5ef70700946ddca56fd301a0f7a40230a06
Jonathan Lau <jonhlau28@gmail.com>
2019-05-02 20:03:12 -0700

    add base mutation with base mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,9 +2,9 @@
 
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-                               description: 'An example field added by the generator'
+    field :test_field, String, null: false, description: 'An example field added by the generator'
+    field :create_channel, mutation: Mutations::CreateChannel
+
     def test_field
       'Hello World'
     end
