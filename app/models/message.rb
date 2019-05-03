d6eeff9476cf8ef5dc990b63736e5389b125f08a
Jonathan Lau <jonhlau28@gmail.com>
2019-05-02 21:15:47 -0700

    Configure current user check on window

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,11 @@ class Message < ApplicationRecord
   validates :body, :user_id, :channel_id, presence: true 
   belongs_to :user
   belongs_to :channel
+
+  after_create :notify_pusher, on: :create
+
+  def notify_pusher
+    Pusher.trigger('message', 'new', self.as_json)
+  end
+
 end
